4efa3f415791f9b81863a97e1603327a567abfde
jaocruz <mcruznq@gmail.com>
2024-10-04 16:01:07 -0300

    global css

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+   }
+
+   body {
+      color: ${props => props.theme["base-text"]};
+      background: ${props => props.theme.background};
+   }
+
+   body, input, textarea, button {
+      font-size: 1rem;
+      font-weight: 400;
+      font-family: 'Roboto', sans-serif;
+   }
+`
